0f5e16aaf6c402df8122466df979b5505b550237
LeoPavlovski01 <leopavlovski01@gmail.com>
2023-06-09 21:28:46 +0200

    MatchesController, Routes, Crud operations

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -11,7 +11,7 @@ class Game extends Model
 
     protected $table = 'matches';
 
-    protected $fillabe = [
+    protected $fillable = [
       'team_1',
       'team_2',
        'score',
--- routes/api.php
+++ routes/api.php
@@ -23,11 +23,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function(){
 Route::get('/listMatches',[MatchController::class, 'index']);
-Route::post('/createTeam',[TeamController::class , 'create']);
-Route::post('/updateTeam/{team}',[TeamController::class, 'update']);
+Route::post('/createTeam',[TeamController::class , 'store']);
+Route::put('/updateTeam/{team}',[TeamController::class, 'update']);
 Route::delete('/deleteTeam/{delete}',[TeamController::class ,'destroy']);
-Route::post('/createMatch', [MatchController::class, 'create']);
-
+Route::post('/createMatch', [MatchController::class, 'store']);
+Route::put('/updateMatch/{game}',[MatchController::class, 'update']);
+Route::delete('/deleteMatch/{game}',[MatchController::class,'destroy']);
+Route::get('/getMatches',[MatchController::class, "index"]);
+Route::get('/getMatch/{game}',[MatchController::class, 'show']);
 });
+
 Route::post('/auth/register', [AuthenticationController::class, 'registerUser']);
 Route::post('/auth/login',[AuthenticationController::class, 'loginUser']);
